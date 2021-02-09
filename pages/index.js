ba0fa7818d60e30b9c865b170e446346b00b1d32
Bengin Cetindere <bengin@cetindere.de>
2021-02-09 13:36:07 +0100

    feature: add drop shadows

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         
         {/*About Section*/}
         <div className="bg-primary-darker pb-12">
-            <div className="bg-primary-dark text-primary-white p-12 pt-24 lg:pt-12 mx-auto max-w-7xl xl:rounded-xl">
+            <div className="bg-primary-dark text-primary-white p-12 pt-24 lg:pt-12 mx-auto max-w-7xl xl:rounded-xl shadow-md">
                 <div className="relative">
                     <div className="absolute right-1/2 transform translate-x-1/2 -top-40 lg:-top-28 xl:right-16 xl:translate-x-0 transition-all hover:-translate-y-2 duration-300">
                         <img
@@ -52,7 +52,7 @@ export default function Home() {
             <h2 className="text-5xl font-bold text-primary-bold text-center mb-8">What I work on</h2>
             <div className="mx-auto text-center text-primary-font max-w-7xl">
                 
-                <div className="md:flex md:items-stretch p-12 xl:rounded-lg bg-primary-dark mb-16">
+                <div className="md:flex md:items-stretch p-12 xl:rounded-lg bg-primary-dark mb-16 shadow-md">
                     <div className="mb-8 md:mx-8 md:order-2 md:w-2/3 flex flex-col justify-between">
                         <div className="flex-1 flex flex-col justify-center">
                             <h3 className="text-4xl font-bold text-primary-bold mb-4">Subscribee</h3>
@@ -63,10 +63,10 @@ export default function Home() {
                             </p>
                             <div className="flex justify-evenly p-8">
                                 <a href="https://subscribee.net" target="_blank" rel="noreferrer">
-                                    <i className="fas fa-link bg-accent p-3 rounded-md text-primary-white"/>
+                                    <i className="fas fa-link bg-accent p-3 rounded-md text-primary-white shadow-md"/>
                                 </a>
                                 <a href="https://twitter.com/subscribee_net" target="_blank" rel="noreferrer">
-                                    <i className="fab fa-twitter bg-accent p-3 rounded-md text-primary-white"/>
+                                    <i className="fab fa-twitter bg-accent p-3 rounded-md text-primary-white shadow-md"/>
                                 </a>
                             </div>
                         </div>
@@ -75,7 +75,7 @@ export default function Home() {
                     <img src="/subscribee_mobile.png" className="md:w-1/3 rounded-lg shadow-png"/>
                 </div>
 
-                <div className="md:flex md:items-stretch p-12 xl:rounded-lg bg-primary-dark mb-16">
+                <div className="md:flex md:items-stretch p-12 xl:rounded-lg bg-primary-dark mb-16 shadow-md">
                     <div className="md:mx-8 flex flex-col justify-center">
                         <h3 className="text-4xl font-bold text-primary-bold mb-4">Minervabooks</h3>
                         <p>
@@ -85,14 +85,14 @@ export default function Home() {
                         </p>
                         <div className="flex justify-evenly p-8">
                             <a href="https://minervabooks.net" target="_blank" rel="noreferrer">
-                                <i className="fas fa-link bg-accent p-3 rounded-md text-primary-white"/>
+                                <i className="fas fa-link bg-accent p-3 rounded-md text-primary-white shadow-md"/>
                             </a>
                         </div>
                     </div>
                     <img src="/minervabooks.png" className="md:w-2/3 rounded-md shadow-png"/>
                 </div>
 
-                <div className="md:flex md:items-stretch p-12 xl:rounded-lg bg-primary-dark">
+                <div className="md:flex md:items-stretch p-12 xl:rounded-lg bg-primary-dark shadow-md">
                     <div className="md:mx-8 md:order-2 flex flex-col justify-center">
                         <h3 className="text-4xl font-bold text-primary-bold mb-4">My Personal Blog</h3>
                         <p>
@@ -101,10 +101,10 @@ export default function Home() {
                         </p>
                         <div className="flex justify-evenly p-8">
                             <a href="https://blog.cetindere.de" target="_blank" rel="noreferrer">
-                                <i className="fas fa-link bg-accent p-3 rounded-md text-primary-white"/>
+                                <i className="fas fa-link bg-accent p-3 rounded-md text-primary-white shadow-md"/>
                             </a>
                             <a href="https://twitter.com/_divby0" target="_blank" rel="noreferrer">
-                                <i className="fab fa-twitter bg-accent p-3 rounded-md text-primary-white"/>
+                                <i className="fab fa-twitter bg-accent p-3 rounded-md text-primary-white shadow-md"/>
                             </a>
                         </div>
                     </div>
